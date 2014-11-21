4bdf193517c7771ec3f056b0aa156be4f582c4a8
Daniel Ochoa <daniel@poeticsystems.com>
2014-11-21 10:51:12 -0600

    remove treeForStyles for now until I figure out why it is not working.

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
+'use strict';
+
 var path = require('path');
 
 module.exports = {
-  name: 'ember-cli-proto-css',
-
-  treeForStyles: function(tree) {
-    var singularityPath = path.join(this.app.bowerDirectory, 'singularity', 'stylesheets');
-    var singularityTree = this.pickFiles(this.treeGenerator(singularityPath), {
-      srcDir: '/',
-      destDir: '/app/styles'
-    });
-    return this.mergeTrees([tree, singularityTree]);
-  }
+  name: 'ember-cli-proto-css'
 };
